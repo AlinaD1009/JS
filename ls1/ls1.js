b13626682cd4d34421f428fa4becfc843a750d56
AlinaDirkach <alina.dirkach1009@gmail.com>
2024-09-29 06:45:01 +0200

    Add files via upload

---
+++ ls1/ls1.js
@@ -0,0 +1,60 @@
+var varA = "CONSOLE.LOG var"
+var varB
+var varC = "ALERT var"
+var varD = "CONFIRM var"
+var varE = "PROMT var"
+
+console.log(varA)
+alert(varC)
+confirm(varD)
+prompt(varE)
+
+varB = "Декларация - это объявление переменной. Объявление позволяет получить доступ к переменной позже в коде"
+
+let letA 
+let letB
+let letC
+let letD
+let letE
+
+letA = "CONSOLE.LOG let"
+letB = "Инициализация - это присвоение переменной значения."
+letC = "ALERT let"
+letD = "CONFIRM let"
+letE = "PROMT let"
+
+console.log(letA)
+
+alert(letC)
+confirm(letD)
+prompt(letE)
+
+const CONST_A = "CONSOLE.LOG const"
+const CONST_B = "Операторы var, let и const — это три инструмента для объявления и инициализации переменных в языке программирования JavaScript."
+const CONST_C = "ALERT const"
+const CONST_D = "CONFIRM const"
+const CONST_E = "PROMT const"
+
+console.log(CONST_A)
+alert(CONST_C)
+confirm(CONST_D)
+prompt(CONST_E)
+
+
+
+document.write(CONST_B)
+document.write("<hr>")
+document.write("Ключевое слово var в JavaScript используется для объявления переменной и опционального присвоения ей значения. Однако, начиная с ECMAScript 6, вместо var часто используются let и const для объявления переменных, которые обеспечивают еще больший контроль над областью видимости переменных.")
+document.write("<hr>")
+document.write("var -переменные могут быть как обновлены, так и переопределены внутри области видимости;")
+document.write("<hr>")
+document.write("let -переменные можно обновлять, но не переопределять;")
+document.write("<hr>")
+document.write("const -переменные нельзя ни обновлять, ни переопределять.")
+document.write("<hr>")
+document.write(varB)
+document.write("<hr>")
+document.write(letB)
+
+
+
